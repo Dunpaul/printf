564e995f9ca1c947f946f20b9d69454986e1dd89
fgalulu <fionasonny132@gmail.com>
2022-04-21 18:47:24 +0000

    tasks

--- main.h
+++ main.h
@@ -1,7 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_int(va_list arg);
+int print_char(va_list arg);
+int print_str(va_list arg);
+
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif
